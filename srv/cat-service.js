b7bf2d6ed882db4ba5858c5b0f26b06ba509b780
Fred Verheul <fred.verheul@gmail.com>
2019-05-24 08:28:34 +0200

    Added AFTER READ exit to implementation

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -1,5 +1,10 @@
 module.exports = srv => {
 
     console.log('Service name:', srv.name)
-  
+
+    if (srv.name === 'CatalogService') {
+        srv.after ('READ', 'Books', each => {
+          if (each.stock > 500) each.title = '(5% off!) ' + each.title
+        })
+      }
   }
